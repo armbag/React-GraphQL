5b15291a0ccbad29fd5d158dedda8d83ac5e81ab
Armen <armen_bagramian@gmail.com>
2020-09-29 14:37:44 +0200

    added graphql to config.js

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444/graphql`
-export const prodEndpoint = `https://new-51ck.herokuapp.com/`
+export const prodEndpoint = `https://new-51ck.herokuapp.com/graphql`
 
 export const perPage = 4
